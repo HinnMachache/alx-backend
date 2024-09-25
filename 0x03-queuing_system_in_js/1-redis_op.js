80ebf2e838e42c27deaac73356171f591b2e9ef2
HinnMachache <bennyhinn253.bh@gmail.com>
2024-09-25 13:36:06 -0700

    Redis Cache

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+import { print, createClient } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('error', (error) => {
+  console.log(`Redis client not connected to server: ${error.message}`);
+  redisClient.quit();
+});
+
+redisClient.on('connect', () => console.log('Redis client connected to the server'));
+
+console.log(redisClient.connected);
+/**
+ * Set a key-value pair in redis
+ * @param {string} schoolName - key
+ * @param {string} value      - value
+ */
+function setNewSchool(schoolName, value) {
+  redisClient.set(schoolName, value, print);
+}
+
+/**
+ * retrieves and display the value associated
+* with given key in the store.
+ * @param {string} schoolName - key to search in the redis store
+ */
+function displaySchoolValue(schoolName) {
+  redisClient.get(schoolName, (_error, value) => {
+    if (value) console.log(value);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ 0x03-queuing_system_in_js/README.md
@@ -0,0 +1,1 @@
+Reddis: Cache memory in Nodejs
